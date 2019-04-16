3ba279ecb9755273574f740e4c410457dba1e205
adambrgmn <adam@wordpressutvecklare.se>
2019-04-16 09:21:23 +0200

    Adding localStorage handling for projects

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,10 +4,16 @@ import AddProject from '../projects/AddProject'
 
 class Home extends Component {
   state = {
-    projects: [
-      {id: 1, name: '1dv430'},
-      {id: 2, name: '1dv024'}
-    ]
+    projects: []
+  }
+
+  componentWillMount() {
+    if (localStorage.hasOwnProperty('project')) {
+      const projects = JSON.parse(localStorage.getItem('project'))
+      this.setState({
+        projects
+      })
+    }
   }
 
   deleteProject = (id) => {
@@ -15,12 +21,21 @@ class Home extends Component {
       return project.id !== id
     })
     this.setState({ projects })
+
+    const dataInStorage = JSON.parse(localStorage.getItem('project'))
+    const filteredItem = [...dataInStorage.filter(project => project.id !== id)]
+    localStorage.removeItem('project')
+    localStorage.setItem('project', JSON.stringify(filteredItem))
   }
 
   addProject = (project) => {
     project.id = Math.random()
     let projects = [...this.state.projects, project]
     this.setState({ projects })
+
+    // Save the added task to localStorage together with the existing
+    const allProjects = [...this.state.projects, project]
+    localStorage.setItem('project', JSON.stringify(allProjects))
   }
 
 
