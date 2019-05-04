cb04b35ffcd11758a3c570084b917f9d687caca3
adambrgmn <adam@wordpressutvecklare.se>
2019-05-04 15:44:37 +0200

    Adding tests for date formatting

--- src/lib/crudHelpers.test.js
+++ src/lib/crudHelpers.test.js
@@ -2,84 +2,104 @@
 
 import { addItemToArray, findItemInArray, deleteItemFromArray, saveToLocalStorage, removeFromLocalStorage, findInLocalStorage } from './crudHelpers'
 
-test('addItemToArray should add the passed item to the list', () => {
-  const existingArray = [
-    { name: 'two', rate: '200', id: '2' },
-    { name: 'one', rate: '100', id: '1' }
-  ]
-  const newItem = { name: 'three', rate: '300', id: '3' }
+describe('Add, find & delete with arrays', () => {
+  test('addItemToArray should add the passed item to the list', () => {
+    const existingArray = [
+      { name: 'two', rate: '200', id: '2' },
+      { name: 'one', rate: '100', id: '1' }
+    ]
+    const newItem = { name: 'three', rate: '300', id: '3' }
 
-  const expected = [
-    { name: 'three', rate: '300', id: '3' },
-    { name: 'two', rate: '200', id: '2' },
-    { name: 'one', rate: '100', id: '1' }
-  ]
-  const result = addItemToArray(newItem, existingArray)
-  expect(result).toEqual(expected)
+    const expected = [
+      { name: 'three', rate: '300', id: '3' },
+      { name: 'two', rate: '200', id: '2' },
+      { name: 'one', rate: '100', id: '1' }
+    ]
+    const result = addItemToArray(newItem, existingArray)
+    expect(result).toEqual(expected)
+  })
+
+  test('addItemToArray should not mutate the existing array', () => {
+    const existingArray = [
+      { name: 'two', rate: '200', id: '2' },
+      { name: 'one', rate: '100', id: '1' }
+    ]
+    const newItem = { name: 'three', rate: '300', id: '3' }
+    const result = addItemToArray(newItem, existingArray)
+
+    expect(result).not.toBe(existingArray)
+  })
+
+  test('findItemInArray should return the expected item from an array', () => {
+    const existingArray = [
+      { name: 'two', rate: '200', id: '2' },
+      { name: 'one', rate: '100', id: '1' }
+    ]
+    const expected = [{ name: 'two', rate: '200', id: '2' }]
+    const result = findItemInArray('2', existingArray)
+
+    expect(result).toEqual(expected)
+  })
+
+  test('findItemInArray should not mutate the existing array', () => {
+    const existingArray = [
+      { name: 'two', rate: '200', id: '2' },
+      { name: 'one', rate: '100', id: '1' }
+    ]
+    const result = findItemInArray('2', existingArray)
+
+    expect(result).not.toBe(existingArray)
+  })
+
+  test('deleteItemFromArray should remove an item by id', () => {
+    const existingArray = [
+      { name: 'two', rate: '200', id: '2' },
+      { name: 'one', rate: '100', id: '1' }
+    ]
+    const expected = [{ name: 'one', rate: '100', id: '1' }]
+    const result = deleteItemFromArray('2', existingArray)
+
+    expect(result).toEqual(expected)
+  })
+
+  test('deleteItemFromArray should not mutate the original array', () => {
+    const existingArray = [
+      { name: 'two', rate: '200', id: '2' },
+      { name: 'one', rate: '100', id: '1' }
+    ]
+    const result = deleteItemFromArray('2', existingArray)
+    expect(result).not.toBe(existingArray)
+  })
 })
 
-test('addItemToArray should not mutate the existing array', () => {
-  const existingArray = [
-    { name: 'two', rate: '200', id: '2' },
-    { name: 'one', rate: '100', id: '1' }
-  ]
-  const newItem = { name: 'three', rate: '300', id: '3' }
+describe('localStorage', () => {
+  test('saveToLocalStorage should save an array to localStorage', () => {
+    const testArray = [{ name: 'Test project', rate: '500', id: '001' }]
+    saveToLocalStorage('test', testArray)
+    const savedInLocalStorage = JSON.parse(window.localStorage.getItem('test'))
 
-  const result = addItemToArray(newItem, existingArray)
+    expect(testArray).toEqual(savedInLocalStorage)
+  },
+  window.localStorage.removeItem('test')
+  )
 
-  expect(result).not.toBe(existingArray)
-})
+  test('findInLocalStorage should return data stored in localStorage', () => {
+    const testArray = [{ name: 'Test project', rate: '500', id: '001' }]
+    window.localStorage.setItem('test', JSON.stringify(testArray))
+    const result = findInLocalStorage('test')
 
-test('findItemInArray should return the expected item from an array', () => {
-  const existingArray = [
-    { name: 'two', rate: '200', id: '2' },
-    { name: 'one', rate: '100', id: '1' }
-  ]
-  const expected = [{ name: 'two', rate: '200', id: '2' }]
+    expect(testArray).toEqual(result)
+  },
+  window.localStorage.removeItem('test')
+  )
 
-  const result = findItemInArray('2', existingArray)
+  test('removeFromLocalStorage should remove a key from localStorage', () => {
+    const testArray = [{ name: 'Test project', rate: '500', id: '001' }]
+    window.localStorage.setItem('test', JSON.stringify(testArray))
+    const savedToLocal = JSON.parse(window.localStorage.getItem('test'))
 
-  expect(result).toEqual(expected)
-})
+    removeFromLocalStorage('test')
 
-test('deleteItemFromArray should remove an item by id', () => {
-  const existingArray = [
-    { name: 'two', rate: '200', id: '2' },
-    { name: 'one', rate: '100', id: '1' }
-  ]
-  const expected = [{ name: 'one', rate: '100', id: '1' }]
-
-  const result = deleteItemFromArray('2', existingArray)
-
-  expect(result).toEqual(expected)
-})
-
-test('deleteItemFromArray should not mutate the original array', () => {
-  const existingArray = [
-    { name: 'two', rate: '200', id: '2' },
-    { name: 'one', rate: '100', id: '1' }
-  ]
-  const result = deleteItemFromArray('2', existingArray)
-
-  expect(result).not.toBe(existingArray)
-})
-
-test('saveToLocalStorage should save an array to localStorage', () => {
-  const testArray = [{ name: 'Test project', rate: '500', id: '001' }]
-
-  saveToLocalStorage('test', testArray)
-  const savedInLocalStorage = JSON.parse(window.localStorage.getItem('test'))
-
-  expect(testArray).toEqual(savedInLocalStorage)
-},
-window.localStorage.removeItem('test')
-)
-
-test('findInLocalStorage should return data stored in localStorage', () => {
-  const testArray = [{ name: 'Test project', rate: '500', id: '001' }]
-
-  window.localStorage.setItem('test', JSON.stringify(testArray))
-  const result = findInLocalStorage('test')
-
-  expect(testArray).toEqual(result)
+    expect(window.localStorage).not.toBe(savedToLocal)
+  })
 })
---
+++ src/lib/dateHelpers.test.js
@@ -0,0 +1,22 @@
+import { getElapsedTime, getShortDate } from './dateHelpers'
+
+/* eslint-env jest */
+
+describe('Date formatting', () => {
+  test('getElapsedTime should return the time in format HH:MM:SS', () => {
+    const elapsed = 5004
+    const result = getElapsedTime(elapsed)
+
+    const expected = '00:00:05'
+
+    expect(result).toEqual(expected)
+  })
+
+  test('getShortDate should return a date in the format DD/MM/YY', () => {
+    const date = 'Sat May 04 2019 15:41:13 GMT+0200 (centraleuropeisk sommartid)'
+    const result = getShortDate(date)
+    const expected = '4/5/2019'
+
+    expect(result).toEqual(expected)
+  })
+})
