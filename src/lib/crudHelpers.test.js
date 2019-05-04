30f7b8af15091f06b849b55be7429fb133dc344f
adambrgmn <adam@wordpressutvecklare.se>
2019-05-04 14:45:56 +0200

    Implementing testing for filtering an array

--- src/lib/crudHelpers.test.js
+++ src/lib/crudHelpers.test.js
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 
-import { addItemToArray } from './crudHelpers'
+import { addItemToArray, findItemInArray } from './crudHelpers'
 
 test('addItemToArray should add the passed item to the list', () => {
   const existingArray = [
@@ -28,4 +28,16 @@ test('addItemToArray should not mutate the existing array', () => {
   const result = addItemToArray(newItem, existingArray)
 
   expect(result).not.toBe(existingArray)
+})
+
+test('findItemInArray should return the expected item from an array', () => {
+  const existingArray = [
+    { name: 'two', rate: '200', id: '2' },
+    { name: 'one', rate: '100', id: '1' }
+  ]
+  const expected = [{ name: 'two', rate: '200', id: '2' }]
+
+  const result = findItemInArray(existingArray[0].id, existingArray)
+
+  expect(result).toEqual(expected)
 })
